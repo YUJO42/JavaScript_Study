5a4cb06b3bdd8b9e8788e413fa3d536b8ae0e330
yujo <yujo@student.42seoul.kr>
2020-08-19 15:13:28 +0900

    null & undefined

---
+++ Core_JavaScript/01_Data_Type/undefined_null.js
@@ -0,0 +1,14 @@
+// let a;
+// console.log(a);
+
+// const obj = { a: 1 };
+// console.log(obj.a);
+// console.log(obj.b);
+
+// const func = () => {};
+// const c = func();
+// console.log(c);
+
+const arr = [];
+arr.length = 3;
+console.log(arr);
