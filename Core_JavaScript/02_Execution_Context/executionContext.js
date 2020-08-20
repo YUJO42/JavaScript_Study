d8989359a7ddf1e22c09b9ae9fa886a6b355398e
yujo <yujo@student.42seoul.kr>
2020-08-20 15:22:53 +0900

    study JS Execution Context

---
+++ Core_JavaScript/02_Execution_Context/executionContext.js
@@ -0,0 +1,14 @@
+//------------------------(1)
+var a = 1;
+
+function outer() {
+  function inner() {
+    console.log(a);
+    var a = 3;
+  }
+  inner(); // -------------(2)
+  console.log(a);
+}
+
+outer(); // --------------(3)
+console.log(a);
