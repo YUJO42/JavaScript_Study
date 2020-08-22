d81d8af347ecfb2629f02ab17057ca5b563cb4cb
yujo <yujo@student.42seoul.kr>
2020-08-23 08:18:11 +0900

    CodeFestival JS 01 ~ 05 solved

---
+++ CodeFestival_JS/yujo/completed/01.js
@@ -0,0 +1,10 @@
+/* 다음 배열에서 400, 500을 삭제하는 code를 입력하세요.
+	let nums = [100, 200, 300, 400, 500]; */
+
+let nums = [100, 200, 300, 400, 500];
+
+// initial answer
+nums.splice(3, 2);
+
+// check answer
+console.log(nums);
---
+++ CodeFestival_JS/yujo/completed/03.js
@@ -0,0 +1,25 @@
+/* 다음 출력값으로 올바른 것은?
+    
+    var arr = [100, 200, 300];
+    console.log(typeof arr);
+    
+    1) undefined
+    2) string
+    3) number
+    4) object
+*/
+
+// my answer : 4) object
+
+// check answer
+var arr = [100, 200, 300];
+console.log(typeof arr);
+
+/* Javascript Primitive Type
+    - boolean
+    - number
+    - string
+    - null
+    - undefined
+    - symbol
+*/
